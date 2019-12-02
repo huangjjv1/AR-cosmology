3029068df9e93d39c957bad4c2a8faaf3414231a
hwhv66 <zengw23@gmail.com>
2019-12-02 20:22:01 +0000

    Detail and Hint View added

--- ARImageDetection/DetailViewController.swift
+++ ARImageDetection/DetailViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var imageLabel: UIImageView!
     @IBOutlet weak var contentLabel: UILabel!
     var nodeName: String!
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageLabel.image = UIImage(named: "art.scnassets/planet.png")
-        contentLabel.text = "This is " + nodeName
+        titleLabel.text = "Detail of the model"
+        imageLabel.image = UIImage(named: "art.scnassets/Targets/" + nodeName + ".png")
+        if (nodeName == "") {
+            contentLabel.text = ""
+        }
+        
     }
     @IBAction func closeButtonTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
